08ab6da67b7e19680bab481131bcb0d75cccf653
ALopukhin <79193398186l@gmail.com>
2024-10-03 14:21:08 +0300

    Добавил микросервис авторизации, в нем регистрацию, создание токена игры с игроками и идентификатором игры. В агент добавил метод регистрации в сервисе авторизации и создание игры через получение токена и самого создания на игровом сервере

--- AuthorizationServer/src/test/java/ru/spacebattle/authorizationserver/AuthorizationServerApplicationTests.java
+++ AuthorizationServer/src/test/java/ru/spacebattle/authorizationserver/AuthorizationServerApplicationTests.java
@@ -1,10 +1,6 @@
 package ru.spacebattle.authorizationserver;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.junit.jupiter.api.Testy;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest()
